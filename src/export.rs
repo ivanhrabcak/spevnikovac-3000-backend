fa529ddfc80c2e792daeb9f7087c8408fbf853bb
Ivan Hrabcak <ivan@hrabcak.eu>
2024-08-27 17:44:34 +0200

    Add the `get_editing_hints` command

--- src/export.rs
+++ src/export.rs
@@ -1,16 +1,64 @@
+use itertools::Itertools;
 use scraper::Html;
+use serde::{Deserialize, Serialize};
 
 use crate::domain::{
-    core::{LyricsWithChords, Source},
+    core::{LyricsWithChords, Source, TextNode},
     ultimate_guitar::UltimateGuitar,
 };
 
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum EditingHint {
+    Node(TextNode),
+    PossibleChordPlace,
+}
+
 #[tauri::command(async)]
-pub async fn fetch(url: String) -> LyricsWithChords {
+pub async fn fetch(url: String) -> Result<LyricsWithChords, String> {
     let client = reqwest::Client::new();
 
-    let text = client.get(url).send().await.unwrap().text().await.unwrap();
+    let response = match client.get(url).send().await {
+        Ok(r) => r,
+        Err(e) => return Err(e.to_string()),
+    };
+
+    let text = match response.text().await {
+        Ok(t) => t,
+        Err(e) => return Err(e.to_string()),
+    };
 
     let document = Html::parse_document(&text);
-    UltimateGuitar::get(&document, None).unwrap()
+
+    UltimateGuitar::get(&document, None).map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+pub fn get_editing_hints(nodes: Vec<TextNode>) -> Vec<EditingHint> {
+    nodes
+        .iter()
+        .flat_map(|node| match node {
+            TextNode::Text(t) => {
+                let parts: Vec<&str> = t.split(" ").collect();
+
+                parts
+                    .iter()
+                    .flat_map(|part| {
+                        vec![
+                            EditingHint::Node(TextNode::Text(part.to_string())),
+                            EditingHint::PossibleChordPlace,
+                        ]
+                    })
+                    .take(parts.len() * 2 - 1)
+                    .collect::<Vec<EditingHint>>()
+            }
+            TextNode::Chord(_) => vec![
+                EditingHint::PossibleChordPlace,
+                EditingHint::Node(node.clone()),
+                EditingHint::PossibleChordPlace,
+            ],
+            TextNode::Label(_) => vec![EditingHint::Node(node.clone())],
+            TextNode::Newline => vec![EditingHint::Node(node.clone())],
+        })
+        .dedup()
+        .collect()
 }
